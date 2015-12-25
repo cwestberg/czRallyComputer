afe2552b5fd11ab8b47dc3f12d38340445154342
Clarence Westberg <clarence.westberg@gmail.com>
2015-12-25 07:38:02 -0600

    ITS A START

---
+++ CZRallyComputer/CZDataViewController.swift
@@ -0,0 +1,68 @@
+//
+//  CZDataViewController.swift
+//  CZRallyComputer
+//
+//  Created by Clarence Westberg on 12/24/15.
+//  Copyright © 2015 Clarence Westberg. All rights reserved.
+//
+
+import UIKit
+
+class CZDataViewController: UIViewController {
+
+    @IBOutlet weak var controlNumberField: UITextField!
+    @IBOutlet weak var speedTextField: UITextField!
+    @IBOutlet weak var hourField: UITextField!
+
+    @IBOutlet weak var minuteField: UITextField!
+
+    @IBOutlet weak var timeUnitsField: UITextField!
+
+    var controlNumber: Int!
+    var speed: Int!
+    var speedd: Double!
+    var hour: Int!
+    var minute: Int!
+    var second: Int!
+    var startTime: NSDate?
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let currentDate = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: currentDate)
+        controlNumberField.text = "\(self.controlNumber)"
+        self.speedTextField.text = "\(self.speed)"
+        self.hourField.text = "\(dateComponents.hour)"
+        self.minuteField.text = "\(dateComponents.minute)"
+        //        self.secondField.text = "\(dateComponents.second)"
+        self.timeUnitsField.text = "00"
+        
+    }
+
+    @IBAction func doneBtn(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        //        let currentDate = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: NSDate())
+        //        let dateString = "2015-12-21 18:51:00"
+        //        let date: NSDate? = dateFormatter.dateFromString(dateString)
+        //        print("date \(date)")
+        
+        //        self.startTime = dateFormatter.dateFromString(dateString)
+        print("st \(self.startTime)")
+        self.startTime = dateFormatter.dateFromString("\(dateComponents.year)-\(dateComponents.month)-\(dateComponents.day) \(self.hourField.text!):\(self.minuteField.text!):00")!
+        
+        
+        
+        self.controlNumber = Int(self.controlNumberField.text!)
+        self.speed = Int(self.speedTextField.text!)
+        self.speedd = Double(self.speedTextField.text!)
+        self.hour = Int(self.hourField.text!)
+        self.minute = Int(self.minuteField.text!)
+        self.second = Int(self.timeUnitsField.text!)
+    }
+
+}
--- CZRallyComputer/ViewController.swift
+++ CZRallyComputer/ViewController.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    
+    var speedd = 45.0
+    var speed = 45
+    var controlNumber = 1
+    var controlZones = [NSManagedObject]()
+    var startTime: NSDate?
+    var todTimer = NSTimer()
+    var timeUnit = "seconds"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,37 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    Segue Stuff
+    @IBAction func unwindToViewController(sender: UIStoryboardSegue){
+        print("unwindSegue \(sender)")
+        
+        if(sender.sourceViewController.isKindOfClass(CZDataViewController))
+        {
+            let dvc = sender.sourceViewController as? CZDataViewController
+            print("\(dvc!.controlNumber)")
+            print("\(dvc!.speed)")
+            print("\(dvc!.speedd)")
+            print("\(dvc!.hour)")
+            print("\(dvc!.minute)")
+            print("\(dvc!.second)")
+            print("\(dvc!.startTime)")
+//            self.saveCZ(dvc!.controlNumber,speed: dvc!.speed,startTime: dvc!.startTime!)
+//            self.tableView.reloadData()
+        }
+        
+    }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "DataSegue"
+        {if let destinationVC = segue.destinationViewController as? CZDataViewController{
+            destinationVC.controlNumber = self.controlNumber
+            destinationVC.speedd = self.speedd
+            destinationVC.speed = self.speed
+            }
+        }
+    }
+    
 
 }
 
