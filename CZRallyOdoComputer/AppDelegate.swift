dc4173ce1af59af6095c0b04a05a53218d0c4756
Clarence Westberg <clarence.westberg@gmail.com>
2015-12-26 15:47:30 -0600

    Adding location controller int to app delegate and added plist entry for navigation

--- CZRallyOdoComputer/AppDelegate.swift
+++ CZRallyOdoComputer/AppDelegate.swift
@@ -13,10 +13,14 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var coreLocationController:CoreLocationController?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.coreLocationController = CoreLocationController()
+        let app = UIApplication .sharedApplication()
+        app.idleTimerDisabled = true
         return true
     }
 
