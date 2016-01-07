0657ceb1f0560b23b45f6feb17716edb7eb99efa
Clarence Westberg <clarence.westberg@gmail.com>
2016-01-07 16:38:03 -0600

    Table removed, first launch

--- CZRallyOdoComputer/ViewController.swift
+++ CZRallyOdoComputer/ViewController.swift
@@ -9,12 +9,11 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
 
     @IBOutlet weak var distanceLbl: UILabel!
     @IBOutlet weak var todLbl: UILabel!
     @IBOutlet weak var ctcLbl: UILabel!
-    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var speedLbl: UILabel!
     @IBOutlet weak var startTimeLbl: UILabel!
     @IBOutlet weak var deltaLbl: UILabel!
@@ -53,7 +52,6 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
         self.todTimer = NSTimer.scheduledTimerWithTimeInterval(0.25, target: self,
             selector: "updateTimeLabel", userInfo: nil, repeats: true)
         
-        self.tableView.registerClass(UITableViewCell.self,forCellReuseIdentifier:"Cell")
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "locationAvailable:", name: "LOCATION_AVAILABLE", object: nil)
         
@@ -90,84 +88,84 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
     
 //    Table Stuff
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        print("count \(controlZones.count)")
-        return controlZones.count
-    }
-    
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//            print("index \(indexPath.row)")
-            let selectedCZ = controlZones[indexPath.row]
-//            print("selectedCZ \(selectedCZ)")
-            let cell = tableView.dequeueReusableCellWithIdentifier("Cell")!
-//            print("\(cell)")
-//            print("\(indexPath.row)")
-            
-            let cz = controlZones[indexPath.row]
-//            print("cz \(cz)")
-            let sm = cz.valueForKey("startDistance")
-            let smStr = String(format: "%.2f", sm as! Float64)
-            
-            let cn = cz.valueForKey("controlNumber")
-//            print("cn \(cn!)")
-            let spd = cz.valueForKey("speedd")
-//            print("spd \(spd!)")
-            let st = self.strippedNSDate(cz.valueForKey("startTime") as! NSDate)
-//            print("st \(st)")
-            cell.textLabel!.text = "\(cn!) \(spd!) \(st) \(smStr)"
-            return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-//        print("You selected cell #\(indexPath.row)!")
-        let selectedCZ = controlZones[indexPath.row]
-//        print("selectedCZ \(selectedCZ)")
-//        print("cn \(selectedCZ.valueForKey("controlNumber")!)")
-//        var cn = "Control \(selectedCZ.valueForKey("controlNumber")!)"
-//        self.controlNumberLbl.text = cn
-        self.speedd = selectedCZ.valueForKey("speedd")! as? Double
-        let selectedSpeed = selectedCZ.valueForKey("speedd")!
-        self.speedLbl.text = String(format: "%.1f",selectedSpeed as! Float64)
-//        self.speed = selectedCZ.valueForKey("speed")! as? Int
-//        self.speedLbl.text = "\(selectedCZ.valueForKey("speed")!)"
-        
-        let sm = selectedCZ.valueForKey("startDistance")!
-        let smStr = String(format: "%.2f", sm as! Float64)
-        self.startDistanceLbl.text = "\(smStr)"
-//        self.startDistanceLbl.text = "\(selectedCZ.valueForKey("startDistance")!)"
-        
-
-        let calendar = NSCalendar.currentCalendar()
-        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: (selectedCZ.valueForKey("startTime") as? NSDate)!)
-        
-        let minStr = String(format: "%02d", dateComponents.minute)
-        let secStr = String(format: "%02d", dateComponents.second)
-        self.startTimeLbl.text = "\(dateComponents.hour):\(minStr):\(secStr)"
-        self.startTime = selectedCZ.valueForKey("startTime") as? NSDate
-        self.selectedStartDistance = (selectedCZ.valueForKey("startDistance")! as? Double)!
-        self.deltaLbl.text = "--"
-    }
-    
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        // 1
-        if editingStyle == .Delete {
-            
-            // 2
-            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-            let moc = appDelegate.managedObjectContext
-            
-            // 3
-            moc.deleteObject(controlZones[indexPath.row])
-            appDelegate.saveContext()
-            
-            // 4
-            controlZones.removeAtIndex(indexPath.row)
-            tableView.reloadData()
-        }
-    }
+//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+////        print("count \(controlZones.count)")
+//        return controlZones.count
+//    }
+//    
+//    func tableView(tableView: UITableView,
+//        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+////            print("index \(indexPath.row)")
+//            let selectedCZ = controlZones[indexPath.row]
+////            print("selectedCZ \(selectedCZ)")
+////            let cell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+////            print("\(cell)")
+////            print("\(indexPath.row)")
+//            
+//            let cz = controlZones[indexPath.row]
+////            print("cz \(cz)")
+//            let sm = cz.valueForKey("startDistance")
+//            let smStr = String(format: "%.2f", sm as! Float64)
+//            
+//            let cn = cz.valueForKey("controlNumber")
+////            print("cn \(cn!)")
+//            let spd = cz.valueForKey("speedd")
+////            print("spd \(spd!)")
+//            let st = self.strippedNSDate(cz.valueForKey("startTime") as! NSDate)
+////            print("st \(st)")
+//            cell.textLabel!.text = "\(cn!) \(spd!) \(st) \(smStr)"
+//            return cell
+//    }
+//    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+////        print("You selected cell #\(indexPath.row)!")
+//        let selectedCZ = controlZones[indexPath.row]
+////        print("selectedCZ \(selectedCZ)")
+////        print("cn \(selectedCZ.valueForKey("controlNumber")!)")
+////        var cn = "Control \(selectedCZ.valueForKey("controlNumber")!)"
+////        self.controlNumberLbl.text = cn
+//        self.speedd = selectedCZ.valueForKey("speedd")! as? Double
+//        let selectedSpeed = selectedCZ.valueForKey("speedd")!
+//        self.speedLbl.text = String(format: "%.1f",selectedSpeed as! Float64)
+////        self.speed = selectedCZ.valueForKey("speed")! as? Int
+////        self.speedLbl.text = "\(selectedCZ.valueForKey("speed")!)"
+//        
+//        let sm = selectedCZ.valueForKey("startDistance")!
+//        let smStr = String(format: "%.2f", sm as! Float64)
+//        self.startDistanceLbl.text = "\(smStr)"
+////        self.startDistanceLbl.text = "\(selectedCZ.valueForKey("startDistance")!)"
+//        
+//
+//        let calendar = NSCalendar.currentCalendar()
+//        let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: (selectedCZ.valueForKey("startTime") as? NSDate)!)
+//        
+//        let minStr = String(format: "%02d", dateComponents.minute)
+//        let secStr = String(format: "%02d", dateComponents.second)
+//        self.startTimeLbl.text = "\(dateComponents.hour):\(minStr):\(secStr)"
+//        self.startTime = selectedCZ.valueForKey("startTime") as? NSDate
+//        self.selectedStartDistance = (selectedCZ.valueForKey("startDistance")! as? Double)!
+//        self.deltaLbl.text = "--"
+//    }
+//    
+//    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+//        // 1
+//        if editingStyle == .Delete {
+//            
+//            // 2
+//            let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+//            let moc = appDelegate.managedObjectContext
+//            
+//            // 3
+//            moc.deleteObject(controlZones[indexPath.row])
+//            appDelegate.saveContext()
+//            
+//            // 4
+//            controlZones.removeAtIndex(indexPath.row)
+//            tableView.reloadData()
+//        }
+//    }
 //    Persistence
-    func saveCZ(controlNumber: Int, speedd: Double, startTime: NSDate, startDistance: Double) {
+    func saveCZ(controlNumber: Int, speedd: Double, startTime: NSDate, startDistance: Double) -> NSManagedObject {
         
         //1
         let appDelegate =
@@ -195,6 +193,7 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
             try managedContext.save()
             //5
             controlZones.append(controlZone)
+            return controlZone
         } catch let error as NSError  {
             print("Could not save \(error), \(error.userInfo)")
         }
@@ -241,9 +240,28 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
             print("\(dvc!.startTime)")
             print("\(dvc!.startDistance)")
             
-            self.saveCZ(dvc!.controlNumber,speedd: dvc!.speedd,startTime: dvc!.startTime!, startDistance: dvc!.startDistance)
-//            self.saveCZ(dvc!.controlNumber,speedd: dvc!.speedd ,speed: dvc!.speed,startTime: dvc!.startTime!, startDistance: dvc!.startDistance)
-            self.tableView.reloadData()
+            let selectedCZ = self.saveCZ(dvc!.controlNumber,speedd: dvc!.speedd,startTime: dvc!.startTime!, startDistance: dvc!.startDistance)
+
+//            let selectedCZ = controlZones[indexPath.row]
+            self.speedd = selectedCZ.valueForKey("speedd")! as? Double
+            let selectedSpeed = selectedCZ.valueForKey("speedd")!
+            self.speedLbl.text = String(format: "%.1f",selectedSpeed as! Float64)
+            
+            let sm = selectedCZ.valueForKey("startDistance")!
+            let smStr = String(format: "%.2f", sm as! Float64)
+            self.startDistanceLbl.text = "\(smStr)"
+            
+            
+            let calendar = NSCalendar.currentCalendar()
+            let dateComponents = calendar.components([NSCalendarUnit.Day, NSCalendarUnit.Month, NSCalendarUnit.Year, NSCalendarUnit.WeekOfYear, NSCalendarUnit.Hour, NSCalendarUnit.Minute, NSCalendarUnit.Second, NSCalendarUnit.Nanosecond], fromDate: (selectedCZ.valueForKey("startTime") as? NSDate)!)
+            
+            let minStr = String(format: "%02d", dateComponents.minute)
+            let secStr = String(format: "%02d", dateComponents.second)
+            self.startTimeLbl.text = "\(dateComponents.hour):\(minStr):\(secStr)"
+            self.startTime = selectedCZ.valueForKey("startTime") as? NSDate
+            self.selectedStartDistance = (selectedCZ.valueForKey("startDistance")! as? Double)!
+            self.deltaLbl.text = "--"
+            
         }
         if(sender.sourceViewController.isKindOfClass(ConfigSegueViewController))
         {
@@ -255,7 +273,7 @@ class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSourc
             if dvc!.clearAllSwitch.on == true {
                 print("Delete!")
                 self.deleteAllData("ControlZone")
-                self.tableView.reloadData()
+//                self.tableView.reloadData()
 
             }
         }
